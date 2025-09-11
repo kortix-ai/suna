072b54c8b8c78195ec0330ed9b2c62e8e8589b70
SundaramManickam <thundermms123@gmail.com>
2025-09-10 21:17:29 -0400

    JSX syntax error

--- frontend/src/components/agents/config/configuration-tab.tsx
+++ frontend/src/components/agents/config/configuration-tab.tsx
@@ -431,7 +431,7 @@ export function ConfigurationTab({
                   </div>
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
