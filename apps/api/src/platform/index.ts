5fd50cd6af59e28af62d5ee32e0a42a02c307b59
marko-kraemer <markokraemer.mail@gmail.com>
2026-06-03 13:25:21 +0200

    chore(api): drop unused platform version route

--- apps/api/src/platform/index.ts
+++ apps/api/src/platform/index.ts
@@ -1,10 +1,8 @@
 import { Hono } from 'hono';
 import { apiKeysRouter } from './routes/api-keys';
-import { versionRouter } from './routes/version';
 
 const platformApp = new Hono();
 
 platformApp.route('/api-keys', apiKeysRouter);
-platformApp.route('/sandbox/version', versionRouter);
 
 export { platformApp };
